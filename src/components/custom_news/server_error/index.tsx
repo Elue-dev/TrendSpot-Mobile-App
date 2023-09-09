b79498a8dfcff7d8b2ea48bb760822c0a6e18cff
Wisdom <eluewisdom@gmail.com>
2023-09-09 12:58:10 +0100

    patch: sdjust server error page layouts

--- src/components/custom_news/server_error/index.tsx
+++ src/components/custom_news/server_error/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import { TouchableOpacity } from "@gorhom/bottom-sheet";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 export default function ServerError({ refetch }: { refetch: () => void }) {
+  const navigation = useNavigation<NavigationProp<any>>();
   return (
     <View
-      className="bg-transparent dark:bg-darkNeutral"
+      className="bg-shadowWhite dark:bg-darkNeutral"
       style={{
         flex: 1,
         justifyContent: "center",
@@ -16,7 +18,8 @@ export default function ServerError({ refetch }: { refetch: () => void }) {
       </Text>
 
       <Text className="font-semibold py-2 text-base text-center w-[90%] text-darkNeutral dark:text-lightText">
-        Try to sign out and sign in again, your session may have expired.
+        Either this resource no longer exists or your session has expired. Try
+        to sign out and sign in again, your session may have expired.
       </Text>
 
       <TouchableOpacity onPress={refetch}>
