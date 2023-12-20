f4e70578e1b1fead02d19f3df9526b3bdd64b033
Wisdom <eluewisdom@gmail.com>
2023-12-20 21:02:56 +0100

    feat: add navigations in web view

--- src/screens/admin_web_view/index.tsx
+++ src/screens/admin_web_view/index.tsx
@@ -1,8 +1,14 @@
-import { useLayoutEffect } from "react";
-import { TouchableOpacity, Text, Platform } from "react-native";
+import { useLayoutEffect, useRef } from "react";
+import {
+  TouchableOpacity,
+  Text,
+  Platform,
+  View,
+  StyleSheet,
+} from "react-native";
 import { NavigationProp } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import { WebView } from "react-native-webview";
 import { COLORS } from "../../common/colors";
 import { useSheet } from "../../context/bottom_sheet/BottomSheetContext";
@@ -10,6 +16,8 @@ import { useSheet } from "../../context/bottom_sheet/BottomSheetContext";
 export default function AdminView() {
   const navigation = useNavigation<NavigationProp<any>>();
   const { isDarkMode } = useSheet();
+  const webViewRef = useRef<any>();
+  const WEB_URL = "https://trend-spot-admin.vercel.app";
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -35,10 +43,120 @@ export default function AdminView() {
     });
   }, [isDarkMode]);
 
+  function webViewGoBack() {
+    if (webViewRef.current) webViewRef.current.goBack();
+  }
+
+  function webViewNext() {
+    if (webViewRef.current) webViewRef.current.goForward();
+  }
+
+  async function reloadWebView() {
+    if (webViewRef.current) webViewRef.current.reload();
+  }
+
   return (
-    <WebView
-      source={{ uri: "https://trend-spot-admin.vercel.app" }}
-      style={{ flex: 1, backgroundColor: "#2A3447" }}
-    />
+    <View style={styles.main}>
+      <WebView
+        source={{ uri: WEB_URL }}
+        style={{ flex: 1, backgroundColor: "#2A3447" }}
+        ref={webViewRef}
+      />
+
+      <View style={styles.tabBarContainer}>
+        <TouchableOpacity
+          onPress={webViewGoBack}
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons
+            name="chevron-back-circle-outline"
+            size={28}
+            color="black"
+          />
+          <Text style={styles.tabText}>Back</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() =>
+            webViewRef.current.injectJavaScript(
+              `
+            window.location.href = '${WEB_URL}';
+          `
+            )
+          }
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Feather name="home" size={24} color="black" />
+          <Text style={styles.tabText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={reloadWebView}
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons name="reload" size={24} color="black" />
+          <Text style={styles.tabText}>Reload</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={webViewNext}
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={28}
+            color="black"
+          />
+          <Text style={styles.tabText}>Forward</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  wrapper: {
+    flex: 1,
+    marginTop: Platform.OS === "android" ? 35 : 1,
+  },
+  tabBarContainer: {
+    height: Platform.OS === "ios" ? 26 : 30,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    justifyContent: "space-between",
+    marginTop: 18,
+    marginBottom: 10,
+  },
+  button: {
+    fontSize: 24,
+  },
+  arrow: {
+    color: "#ef4771",
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  tabText: {
+    textAlign: "center",
+    color: "#111",
+    fontWeight: "600",
+    marginTop: 4,
+  },
+});
